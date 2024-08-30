d13642a62ed608207b725cab2783894f0ca8f195
root <root@shimanuki>
2024-08-30 13:35:25 +0900

    お気に入り機能

--- src/resources/views/list.blade.php
+++ src/resources/views/list.blade.php
@@ -5,42 +5,65 @@
 @endsection
 
 @section('content')
-    <main class="contact-form__main">
-        <div class="toggle-links">
-            <a href="javascript:void(0);" id="toggleRecommended">おすすめ</a>
-            <a href="javascript:void(0);" id="toggleMyList">マイリスト</a>
-        </div>
-
-        <div id="recommendedProducts" class="product-list">
-            @foreach ($recommendedProducts as $recommended)
-                <div class="product-item">
-                    <a href="{{ route('product', ['id' => $recommended['id']]) }}">
-                        <img src="{{ asset($recommended['image']) }}" alt="{{ $recommended['name'] }}">
-                    </a>
+    <main class="product-list">
+        <div class="container">
+            <div class="tab-buttons">
+                <button id="toggleRecommended">おすすめ商品</button>
+                <button id="toggleMyList">マイリスト</button>
+            </div>
+            <!-- おすすめ商品 -->
+            <div id="recommendedProducts" class="product-list__section">
+                <div class="product-list__items">
+                    @foreach ($recommendedProducts as $product)
+                        <div class="product-item">
+                            <a href="{{ route('product', ['id' => $product->id]) }}">
+                                <img src="{{ $product->image_url ? asset($product->image_url) : asset('img/no-image.png') }}" alt="{{ $product->name }}">
+                            </a>
+                        </div>
+                    @endforeach
                 </div>
-            @endforeach
-        </div>
+            </div>
 
-        <div id="myList" class="my-list" style="display: none;">
-            @foreach ($myList as $item)
-                <div class="list-item">
-                    <a href="{{ route('product', ['id' => $item['id']]) }}">
-                        <img src="{{ asset($item['image']) }}" alt="{{ $item['name'] }}">
-                    </a>
+            <!-- マイリスト -->
+            <div id="myList" class="product-list__section" style="display: none;">
+                <div class="product-list__items">
+                    @foreach ($myList as $product)
+                        <div class="product-item">
+                            <a href="{{ $product['link'] }}">
+                                <img src="{{ $product['image_url'] ? asset($product['image_url']) : asset('img/no-image.png') }}" alt="{{ $product['name'] }}">
+                            </a>
+                        </div>
+                    @endforeach
                 </div>
-            @endforeach
+            </div>
+            
         </div>
     </main>
 
     <script>
         document.getElementById('toggleRecommended').addEventListener('click', function() {
-            document.getElementById('recommendedProducts').style.display = 'grid';
+            document.getElementById('recommendedProducts').style.display = 'block';
             document.getElementById('myList').style.display = 'none';
         });
 
         document.getElementById('toggleMyList').addEventListener('click', function() {
             document.getElementById('recommendedProducts').style.display = 'none';
-            document.getElementById('myList').style.display = 'grid';
+            document.getElementById('myList').style.display = 'block';
+        });
+
+        document.querySelectorAll('.favorite-button').forEach(button => {
+            button.addEventListener('click', function() {
+                var productId = this.dataset.productId;
+
+                fetch('/product/' + productId + '/favorite', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                    },
+                    body: JSON.stringify({})
+                }).then(response => response.json())
+            });
         });
     </script>
 @endsection
