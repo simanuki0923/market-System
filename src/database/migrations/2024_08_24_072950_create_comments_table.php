0e4690600e515759641e7621159d27443afcefa1
root <root@shimanuki>
2024-09-04 10:45:23 +0900

    コメント投稿機能として投稿と削除機能を作成しました。

--- src/database/migrations/2024_08_24_072950_create_comments_table.php
+++ src/database/migrations/2024_08_24_072950_create_comments_table.php
@@ -17,7 +17,7 @@ class CreateCommentsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
-            $table->text('comment_text');
+            $table->text('comment');
             $table->timestamps();
         });
     }
