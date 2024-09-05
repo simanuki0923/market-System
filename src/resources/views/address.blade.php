b840004c2acd8e854e83cb03d14dc845ccee8df3
root <root@shimanuki>
2024-09-05 15:39:01 +0900

    配送先住所変更viewを作成しました。

--- src/resources/views/address.blade.php
+++ src/resources/views/address.blade.php
@@ -6,21 +6,18 @@
 
 @section('content')
     <main class="edit-address__main">
-    <h2>住所の変更</h2>
-    <form action="{{ route('address.update') }}" method="POST" class="address-form">
-        <fieldset>
-            <legend>住所情報</legend>
-            <label for="postal_code">郵便番号</label>
-            <input type="text" name="postal_code" id="postal_code" value="{{ $user->postal_code }}" aria-required="true">
+        <h2>住所の変更</h2>
+        <form action="{{ route('purchase.address.update') }}" method="POST" class="address-form">
+            @csrf
+                <label for="postal_code">郵便番号</label>
+                <input type="text" name="postal_code" id="postal_code" value="{{ $profile->postal_code ?? '' }}" aria-required="true">
 
-            <label for="address">住所</label>
-            <input type="text" name="address" id="address" value="{{ $user->address }}" aria-required="true">
+                <label for="address">住所</label>
+                <input type="text" name="address" id="address" value="{{ $profile->address ?? '' }}" aria-required="true">
 
-            <label for="building">建物名</label>
-            <input type="text" name="building" id="building" value="{{ $user->building }}">
-        </fieldset>
-
-        <button type="submit">更新</button>
-    </form>
-</main>
+                <label for="building">建物名</label>
+                <input type="text" name="building" id="building" value="{{ $profile->building ?? '' }}">
+            <button type="submit">更新する</button>
+        </form>
+    </main>
 @endsection
