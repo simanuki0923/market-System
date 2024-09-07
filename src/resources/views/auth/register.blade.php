9153a8249877556072f45e910b1d742ebfaad01b
root <root@shimanuki>
2024-09-07 20:23:59 +0900

    ユーザー登録用view作成しました。

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -9,57 +9,57 @@
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/register.css') }}">
 </head>
-    <header>
-    <a href="/">
-        <img src="{{ asset('img/logo.svg') }}" alt="COACHTECHロゴ">
-    </a>
-    </header>
 <body>
-  <main>
+    <header>
+        <a href="/">
+            <img src="{{ asset('img/logo.svg') }}" alt="COACHTECHロゴ">
+        </a>
+    </header>
+    <main>
         <div class="register__content">
-  <div class="register-form__heading">
-    <h2>会員登録</h2>
-  </div>
-  <form class="form" action="/register" method="post">
-     @csrf
-    <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">メールアドレス</span>
-      </div>
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input type="email" name="email" value="{{ old('email') }}" />
+            <div class="register-form__heading">
+                <h2>会員登録</h2>
+            </div>
+            <form class="form" action="/register" method="post">
+                @csrf
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">メールアドレス</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="email" name="email" value="{{ old('email') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('email')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__group">
+                    <div class="form__group-title">
+                        <span class="form__label--item">パスワード</span>
+                    </div>
+                    <div class="form__group-content">
+                        <div class="form__input--text">
+                            <input type="password" name="password" />
+                        </div>
+                        <div class="form__error">
+                            @error('password')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="form__button">
+                    <button class="form__button-submit" type="submit">登録</button>
+                </div>
+            </form>
+            <div class="login__link">
+                <a class="login__button-submit" href="/login">ログインはこちら</a>
+            </div>
         </div>
-        <div class="form__error">
-          @error('email')
-          {{ $message }}
-          @enderror
-        </div>
-      </div>
-    </div>
-    <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">パスワード</span>
-      </div>
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input type="password" name="password" />
-        </div>
-        <div class="form__error">
-          @error('password')
-          {{ $message }}
-          @enderror
-        </div>
-      </div>
-    </div>
-    <div class="form__button">
-      <button class="form__button-submit" type="submit">登録</button>
-    </div>
-  </form>
-  <div class="login__link">
-    <a class="login__button-submit" href="/login">ログインはこちら</a>
-  </div>
-</div>
     </main>
 </body>
 </html>
