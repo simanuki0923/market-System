2a62aaf6e0f159282d61ab28bafe840e002782d0
root <root@shimanuki>
2024-09-09 10:13:04 +0900

    管理者用権限設定するミドルウェアを作成しました。

---
+++ src/app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        $user = Auth::guard('admins')->user();
+
+        if (!$user || $user->role !== $role) {
+            return redirect('/admin-login');
+        }
+
+        return $next($request);
+    }
+}
