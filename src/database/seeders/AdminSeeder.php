f1ee2cbacf463373b2e9b83c720646120ea2ca69
root <root@shimanuki>
2024-09-09 10:00:03 +0900

    管理者用の権限アカウント作成しました。

---
+++ src/database/seeders/AdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            [
+                'name' => 'admin01',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin001'),
+                'role' => 'admins',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);    
+    }
+}
