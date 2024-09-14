b04f8204898c4e34933df83d767f4f6fee9090d0
root <root@shimanuki>
2024-09-15 00:02:11 +0900

    住所変更用view作成しました。

--- src/resources/views/address.blade.php
+++ src/resources/views/address.blade.php
@@ -16,13 +16,20 @@
         <form action="{{ route('purchase.address.update') }}" method="POST" class="address-form">
             @csrf
                 <label for="postal_code">郵便番号</label>
-                <input type="text" name="postal_code" id="postal_code" value="{{ $profile->postal_code ?? '' }}" aria-required="true">
-
+                <input type="text" name="postal_code" id="postal_code" value="{{ old('postal_code', $profile->postal_code ?? '') }}" aria-required="true">
+                @if ($errors->has('postal_code'))
+                    <p class="error-message">{{ $errors->first('postal_code') }}</p>
+                @endif
                 <label for="address">住所</label>
-                <input type="text" name="address" id="address" value="{{ $profile->address ?? '' }}" aria-required="true">
-
+                <input type="text" name="address" id="address" value="{{ old('address', $profile->address ?? '') }}" aria-required="true">
+                @if ($errors->has('address'))
+                    <p class="error-message">{{ $errors->first('address') }}</p>
+                @endif
                 <label for="building">建物名</label>
-                <input type="text" name="building" id="building" value="{{ $profile->building ?? '' }}">
+                <input type="text" name="building" id="building" value="{{ old('building', $profile->building ?? '') }}">
+                @if ($errors->has('building'))
+                    <p class="error-message">{{ $errors->first('building') }}</p>
+                @endif
             <button type="submit">更新する</button>
         </form>
       </main>
