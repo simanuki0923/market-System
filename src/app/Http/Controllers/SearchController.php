4b2a1d5cd29a3de630ab501de47da99e69df9138
simanuki mamoru <mamoru.simanuki74@gmail.com>
2024-09-16 23:05:05 +0900

    検索機能を実装しました。

--- src/app/Http/Controllers/SearchController.php
+++ src/app/Http/Controllers/SearchController.php
@@ -11,23 +11,15 @@ class SearchController extends Controller
     public function index(Request $request)
     {
         $searchTerm = $request->input('search');
-
-        // Productsの複数カラムで検索
         $products = Product::where('name', 'like', "%{$searchTerm}%")
         ->orWhere('description', 'like', "%{$searchTerm}%")
         ->orWhere('brand', 'like', "%{$searchTerm}%")
         ->orWhere('condition', 'like', "%{$searchTerm}%")
         ->get();
-
-        // Categoriesのnameで検索
         $categories = Category::where('name', 'like', "%{$searchTerm}%")
         ->get();
-
-        // 検索されたCategoriesに属するProductsを取得（カテゴリIDでフィルタリング）
         $categoryProductIds = $categories->pluck('id');
         $categoryProducts = Product::whereIn('category_id', $categoryProductIds)->get();
-
-        // 合わせてProductsとCategoryProductsを結合
         $products = $products->merge($categoryProducts)->unique('id');
 
         return view('search', compact('products', 'categories'));
